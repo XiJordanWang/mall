63935d688d5b3deed9f7d257c66579f32e592759
JordanWang <wangxi009567@sina.com>
2021-04-14 23:12:31 +0800

    添加es6学习代码

---
+++ es6/js/main.js
@@ -0,0 +1,4 @@
+import util from "hello.js";
+import { name, age } from "user.js";
+
+console.log(name);
---
+++ es6/js/user.js
@@ -0,0 +1,8 @@
+var name = 'java';
+var age = 21;
+
+export function add(a, b) {
+    return a + b;
+}
+
+export { name, age }
