e1a550bbdd598cfc14d1cb4caf91bc5937b43d87
JordanWang <wangxi009567@sina.com>
2021-04-17 21:40:08 +0800

    更新前端代码以及spring配置nacos项目

--- back-end/coupon/src/main/java/com/mall/coupon/CouponApplication.java
+++ back-end/coupon/src/main/java/com/mall/coupon/CouponApplication.java
@@ -2,12 +2,13 @@ package com.mall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CouponApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CouponApplication.class, args);
     }
-
 }
